1db150676264727b9264265a5eed272a4c93b186
VikasKare <karevikas7777@gmail.com>
2023-09-13 23:10:41 +0530

    Anonymous Functions

---
+++ Lambda Expression/anonymousFunction4.dart
@@ -0,0 +1,15 @@
+// bool main() {
+//   print("Vikas123456789");
+//   return true;
+// }
+
+// void main(List<String> args) {
+//   print("Vikas Kare");
+// }
+
+
+void main() {
+  void printData = () => print("Vikas Kare");
+
+  printData();
+}
