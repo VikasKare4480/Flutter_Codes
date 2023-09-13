52c89e3a60de836ccd4f041bfa361e0165407cbf
VikasKare <karevikas7777@gmail.com>
2023-09-13 23:45:56 +0530

    Basics of Dart

---
+++ Basics/IO.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter your Name : ");
+  dynamic name = stdin.readLineSync();
+
+  stdout.write("Enter your age : ");
+  var age = stdin.readLineSync();
+
+  print("Hello $name and age : $age");
+}
---
+++ Basics/evenOdd.dart
@@ -0,0 +1,12 @@
+void main() {
+  //
+  void decisionMaker(int a) {
+    if (a % 2 == 0) {
+      print(" $a is even");
+    } else {
+      print("$a is odd");
+    }
+  }
+
+  decisionMaker(10);
+}
